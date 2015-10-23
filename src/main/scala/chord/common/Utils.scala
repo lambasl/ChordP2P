b4235b53317ccd2b2ff0633cd0113d9b6dd6f605
Srinivas Narne <srinivasnarne@outlook.com>
2015-10-23 12:49:44 -0400

    Initial config

--- src/main/scala/chord/common/Utils.scala
+++ src/main/scala/chord/common/Utils.scala
@@ -6,17 +6,16 @@ import java.security.MessageDigest
  * @author user
  */
 object Utils {
-  
-  val m = 7; // size of the ring is 2^7
- 
-  /**
-   * hash(input) calculates the hash of the given input string. Take m bits from sha hash 
-   * of the string and take m bits from it 
-   */
-  def consistentHash(input: String): String ={
-    val sha = MessageDigest.getInstance("SHA-256")
-    var hashedString: String = sha.digest(input.getBytes).foldLeft("")((in: String, b: Byte) => in + Character.forDigit((b & 0xf0) >> 4, 16) + Character.forDigit(b & 0x0f, 16))
 
-    return ""
+  val m = 7; // size of the ring is 2^7
+
+  /**
+   * hash(input) calculates the hash of the given input string. Take m bits from sha hash
+   * of the string and take m bits from it
+   */
+  def consistentHash(input: String): String = {
+    val md = java.security.MessageDigest.getInstance("SHA-1")
+    val hashedString: String = md.digest(input.getBytes("UTF-8")).map("%02x".format(_)).mkString
+    return hashedString
   }
 }
